98b2550f0ca8f251aa43cbe7840a73dc720ab42e
Manoj Mevada <manoj.mevada@gmail.com>
2026-03-17 17:52:21 +0530

    test

--- api/obj/Debug/net10.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net10.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd175aa048bdfab26cf6e79a6b251b1bbfeeecd7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+060ecdaf6a531fdfa536a81b65b233f7cfc7f945")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
