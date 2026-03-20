21102651b32c72d45bfa43bfeafaaab14e7bb53b
Manoj Mevada <manoj.mevada@gmail.com>
2026-03-20 15:01:28 +0530

    self-hosted

--- api/obj/Debug/net10.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net10.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98b2550f0ca8f251aa43cbe7840a73dc720ab42e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0375e07210b3d6d249f7cd205f70bd8fcd14a607")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
